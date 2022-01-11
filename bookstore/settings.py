74e2585143b236c49663737b2cefbae426cca7b4
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2022-01-11 18:33:45 -0300

    Subindo os testes unitarios, wave 1

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'order',
     'product',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
@@ -123,3 +126,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+INTERNAL_IPS = ('127.0.0.1', '192.168.0.1',)
--- order/factories.py
+++ order/factories.py
@@ -1,4 +1,4 @@
-import factory.django
+import factory
 
 from django.contrib.auth.models import User
 from product.factories import ProductFactory
